1602fb01f94e1aec43691e31764761e505e36262
Tisha0513 <tishavarshney13@gmail.com>
2025-04-27 18:23:54 +0600

    loops

--- Module 5 lesson 1/script.js
+++ Module 5 lesson 1/script.js
@@ -2,10 +2,10 @@ function greetings () {
     var greeting;
     var hour = new Date().getHours();
 }
-    if (hour < 11) {
+    if (hour < 12) {
         greeting = "Good Morning"
     }
-    else if (hour < 14) {
+    else if (hour < 15) {
         greeting = "Good Afternoon"
     }
     else {
